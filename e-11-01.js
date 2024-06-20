9975d746875abf8ec46eb901175910ecf6fb6330
TechEdDan2 <dg.nelson2@gmail.com>
2024-06-20 17:37:14 -0400

    Exercise 2 updates

--- e-11-01.js
+++ e-11-01.js
@@ -27,6 +27,35 @@ const PI = 3.14;
 // Exercise 02 - Arrow Functions //
 //                               //
 ///////////////////////////////////
+// ES5 Mapp Callback
+function double(arr) {
+    return arr.map(function (val) {
+        return val * 2;
+    });
+}
+
+/* Write an ES2015 Version */
+//  Refactor the above code to use two arrow functions. Turn it into a one-liner.
+function newDouble(arr) {
+    return arr.map(val => val * 2);
+}
+
+
+
+
+// ES5 Version
+function squareAndFindEvens(numbers) {
+    var squares = numbers.map(function (num) {
+        return num ** 2;
+    });
+    var evens = squares.filter(function (square) {
+        return square % 2 === 0;
+    });
+    return evens;
+}
+
+// Replace ALL functions with arrow functions:
+const newSquareAndFindEvens = numbers => numbers.map(num => num ** 2).filter(square => square % 2 === 0);
 
 ////////////////////////////////////
 //                               //
