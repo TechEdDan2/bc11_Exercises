8b5f3903cf4e0de04d5db3c397b8f924d1f3701d
TechEdDan2 <dg.nelson2@gmail.com>
2024-06-23 22:42:21 -0400

    New Exercise challenges added

--- e-11-01.js
+++ e-11-01.js
@@ -150,3 +150,65 @@ const update = (obj, key, val) => {
     //console.log(newObj);
     return { ...obj, ...newObj }
 }
+
+
+//////////////////////////////////////////
+//                                      //
+// Exercise 03 - Object Enhancements    //
+//                                      //
+//////////////////////////////////////////
+
+/**In this exercise, you’ll refactor some ES5 code into ES2015. Write your code in the sections with a comment to “Write an ES2015 Version”. */
+
+// #1
+function createInstructor(firstName, lastName) {
+    return {
+        firstName: firstName,
+        lastName: lastName
+    }
+}
+
+/* Write an ES2015 Version */
+
+//   #2
+var favoriteNumber = 42;
+
+var instructor = {
+    firstName: "Colt"
+}
+
+instructor[favoriteNumber] = "That is my favorite!"
+
+/* Write an ES2015 Version */
+
+// #3 Object Methods
+var instructor = {
+    firstName: "Colt",
+    sayHi: function () {
+        return "Hi!";
+    },
+    sayBye: function () {
+        return this.firstName + " says bye!";
+    }
+}
+
+/* Write an ES2015 Version */
+
+// #4 createAnimal function
+/** Write a function which generates an animal object. The function should accepts 3 arguments:
+
+- species: the species of animal (‘cat’, ‘dog’)
+- verb: a string used to name a function (‘bark’, ‘bleet’)
+- noise: a string to be printed when above function is called (‘woof’, ‘baaa’)
+
+Use one or more of the object enhancements we’ve covered. */
+
+const d = createAnimal("dog", "bark", "Woooof!")
+// {species: "dog", bark: ƒ}
+d.bark()  //"Woooof!"
+
+const s = createAnimal("sheep", "bleet", "BAAAAaaaa")
+// {species: "sheep", bleet: ƒ}
+s.bleet() //"BAAAAaaaa"
+
+/* Write an ES2015 Version */
