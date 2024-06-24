e828a9ece29de56430c6e672071319b813f89f20
Danny Nelson 2ND <dnel2@Dannys-MacBook-Air.local>
2024-06-24 16:20:25 -0400

    New Folder with img files; some updates to the code

--- e-11-01.js
+++ e-11-01.js
@@ -253,3 +253,60 @@ s.bleet() //"BAAAAaaaa"
 //                                      //
 //////////////////////////////////////////
 
+// Object Destructuring 1 - 3
+// See HTML
+
+// Array Destructuring 1 - 3
+// See HMTL
+
+/** ------------------------------------------------------------------------------- */
+// ES2015 Refactoring - In this exercise, you’ll refactor some ES5 code into ES2015.
+/** ------------------------------------------------------------------------------- */
+
+// # 1 - ES5 Assigning Variables to Object Properties
+var obj = {
+    numbers: {
+        a: 1,
+        b: 2
+    }
+};
+
+var a = obj.numbers.a;
+var b = obj.numbers.b;
+
+/* Write an ES2015 Version */
+
+// # 2 - ES5 Array Swap
+
+var arr = [1, 2];
+var temp = arr[0];
+arr[0] = arr[1];
+arr[1] = temp;
+
+/* Write an ES2015 Version */
+
+// # 3 - ## **raceResults()**
+
+// Write a function called ***raceResults*** which accepts a single array argument. It should return an object with the keys ***first***, ***second***, ***third***, and ***rest***.
+
+// - first: the first element in the array
+// - second: the second element in the array
+// - third: the third element in the array
+// - rest: all other elements in the array
+
+// **Write a *one line* function to make this work using**
+
+// - An arrow function
+// - Destructuring
+// - `Enhanced` object assignment (same key/value shortcut)
+
+raceResults(['Tom', 'Margaret', 'Allison', 'David', 'Pierre'])
+
+/*
+  {
+    first: "Tom",
+    second: "Margaret",
+    third: "Allison",
+    rest: ["David", "Pierre"]
+  }
+*/
